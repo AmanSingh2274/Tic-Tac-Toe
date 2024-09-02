45b98ac366760d06b908013dcd41bf29caa3154e
Aman Singh <148249849+AmanSingh2274@users.noreply.github.com>
2024-09-02 08:23:50 +0530

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,7 +5,7 @@ public class TicTacToe {
         {' ', ' ', ' '},
         {' ', ' ', ' '},
         {' ', ' ', ' '}
-    };
+    }; 
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
